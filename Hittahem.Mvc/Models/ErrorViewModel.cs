e2a9123413c5ba1eb262893a33618bf6da3b3f5c
davidsjoblom <davidsjoblom1@hotmail.com>
2022-03-15 14:52:50 +0100

    Add project files.

---
+++ Hittahem.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hittahem.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
