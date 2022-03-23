ef0d29a2e55cfa83eaf1075bb867730ae1b50f37
davidsjoblom <davidsjoblom1@hotmail.com>
2022-03-23 16:12:47 +0100

    Börjat lägga till lite entitymodels

--- HittaHemMvc.Core/Data/ApplicationDbContext.cs
+++ HittaHemMvc.Core/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace HittaHemMvc.Core.Data
 {
@@ -8,6 +9,13 @@ namespace HittaHemMvc.Core.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //here we put all the normals OnModelCreating stuffs
         }
     }
 }
