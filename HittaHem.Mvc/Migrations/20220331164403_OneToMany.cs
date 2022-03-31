00db506f5fcaeb4c2482a7ad7923140cf47dc756
Imad <imad.yousef79@outlook.com>
2022-03-31 19:09:38 +0200

    added some code

---
+++ HittaHem.Mvc/Migrations/20220331164403_OneToMany.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HittaHem.Mvc.Migrations
+{
+    public partial class OneToMany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "HomeImages",
+                newName: "HomeImageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HomeImageId",
+                table: "HomeImages",
+                newName: "Id");
+        }
+    }
+}
---
+++ HittaHem.Mvc/Migrations/20220331165353_init.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HittaHem.Mvc.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeImages_Homes_HomeId",
+                table: "HomeImages");
+
+            migrationBuilder.RenameColumn(
+                name: "HomeImageId",
+                table: "HomeImages",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HomeId",
+                table: "HomeImages",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeImages_Homes_HomeId",
+                table: "HomeImages",
+                column: "HomeId",
+                principalTable: "Homes",
+                principalColumn: "HomeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HomeImages_Homes_HomeId",
+                table: "HomeImages");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "HomeImages",
+                newName: "HomeImageId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HomeId",
+                table: "HomeImages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HomeImages_Homes_HomeId",
+                table: "HomeImages",
+                column: "HomeId",
+                principalTable: "Homes",
+                principalColumn: "HomeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
